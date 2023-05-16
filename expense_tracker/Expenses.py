86e605b3e55278eb7bd1039c4b36aaa25b53d0ce
Jacek Kowalski <JACEK.KOWALSKI1984@GMAIL.COM>
2023-05-16 23:21:23 +0200

    feat: Add classes for different SQL RDBMS connectors
    Created a parent class, 'Connector', to be inherited by classes representing various SQL connectors (MySQL, SQLite, PostgreSQL). This refactor allows for easy addition of new SQL connector classes without interfering with the main program code.

---
+++ expense_tracker/Expenses.py
@@ -0,0 +1,257 @@
+'''
+Usage:
+    python Expenses.py <command> <command_parametrs...>
+
+Commands and parameters:
+    add <name: str> <amount:float> <category:str> <date:str>
+
+    report: (Optional: category)
+
+Enviromental variables:
+    Note for correct usage enviromental variables for MySQL connection must be set in your terminal:
+
+    export RDBM='rdbm_name' (e.g. 'mysql', 'postgresql', 'sqlite')
+    export HOST='hostname' (e.g. 'localhost')
+    export USER='username' (e.g. 'root')
+    export DATABASE='database_name' (e.g. 'expenses')
+    export PASSWORD='password'
+    export FILENAME='filename' (only for SQLite)
+
+'''
+
+from dataclasses import dataclass
+from datetime import datetime
+import os
+import sys
+
+import click
+from dateutil import parser
+# import mysql.connector
+# from mysql.connector import errorcode
+
+from SQL_Connectors import Connector, MySQL_Connector, PostgreSQL_Connector, SQLite_Connector
+
+
+###    ENVIROMENTAL ARGUMENTS   ###
+RDBM = os.environ.get('RDBM')
+USER= os.environ.get('USER')
+HOST= os.environ.get('HOST')
+PASSWORD = os.environ.get('PASSWORD')
+DB = os.environ.get('DB')
+FILENAME = os.environ.get('FILENAME')
+MYSQL_CONFIG = {
+        'user': USER,
+        'password': PASSWORD,
+        'host': HOST,        
+        'use_pure': False
+        }
+MYSQL_CONFIG_DB= {
+        'user': USER,
+        'password': PASSWORD,
+        'host': HOST,   
+        'database': DB,     
+        'use_pure': False
+        }
+
+# if RDBM == 'mysql':
+#     DB_CONFIG = {
+#         'user': USER,
+#         'password': PASSWORD,
+#         'host': HOST,
+#         'use_pure': False
+#     }
+#     DB_CONFIG_DB = {
+#         'user': USER,
+#         'password': PASSWORD,
+#         'host': HOST,
+#         'database': DATABASE,
+#         'use_pure': False
+#     }
+# elif RDBM == 'postgresql':
+#     DB_CONFIG = {
+#         'user': USER,
+#         'password': PASSWORD,
+#         'host': HOST,
+#         'database': DATABASE
+#     }
+#     DB_CONFIG_DB = DB_CONFIG
+# elif RDBM == 'sqlite':
+#     DB_CONFIG = None
+#     DB_CONFIG_DB = FILENAME
+# else:
+#     raise ValueError('Incorrect RDBM choice.')
+
+######     SQL QUERIES:    ######
+
+Q_CREATE_DATABASE = f'CREATE DATABASE {DB} '
+Q_INSERT_VALUE = '''INSERT INTO expenses_list
+                    VALUES (%s, %s, %s, %s, %s);'''
+Q_SELECT_ALL = 'SELECT * FROM expenses_list'
+Q_SELECT_BY_CATEGORY = 'category = %s'
+Q_DATE_MIN = 'date >'
+Q_DATE_MAX = 'date <'
+
+
+@dataclass
+class Expanse:
+    id: None | int
+    name: str
+    amount : float
+    category: str
+    date: datetime
+
+    def __postinit__ (self):
+        if self.id != None:
+            raise ValueError ('Expanse ID has to be a None value!')
+        if self.name == "":
+            raise ValueError ("Missing name.")
+        if self.amount=="":
+            raise ValueError ("Missing amount argument!")
+        if type(self.amount) != int:
+            raise ValueError ("The amount should be an integer!") 
+        if self.amount <= 0:
+            raise ValueError ("The amount has to be > 0!")
+
+
+def init_connection(rdbm: str|None) -> Connector:
+    if rdbm == 'mysql':
+        return MySQL_Connector(MYSQL_CONFIG_DB)
+    elif rdbm == 'sqlite':
+        return SQLite_Connector(FILENAME)
+    elif rdbm == 'postgresql':
+        return PostgreSQL_Connector(HOST, DB, USER, PASSWORD)
+    else:
+        raise ValueError(f'{RDBM} is an incorrect RDBM choice.')
+
+
+def init_database (conn: Connector)->None:
+    conn.create_database (MYSQL_CONFIG, Q_CREATE_DATABASE)
+
+
+def init_table (conn: Connector)->None:
+    conn.create_table()
+
+
+def close_connection(conn: Connector)->None:
+    conn.connection.close()
+    conn.sqlcursor.close()
+
+
+def create_single_expense (name_:str, amount_: float, category_:str, date_:str)-> Expanse:
+    datetype = parser.parse(date_)
+    return Expanse (
+        id = None,
+        name = name_,
+        amount = float(amount_),
+        category = category_,
+        date = datetype
+        )
+
+
+def insert_values (db: Connector, expense: Expanse):
+    query = Q_INSERT_VALUE
+    data_expense = (None,
+                expense.name,
+                expense.amount,
+                expense.category,
+                expense.date)
+    db.sqlcursor.execute(query, data_expense)
+    db.connection.commit()
+    return db.sqlcursor.fetchall()
+
+
+def add_apostrophes (text: str)-> str:
+    text = f"'{text}'"
+    return text
+
+def convert_date_format (text:str)->str:
+    try: 
+        text_new = parser.parse(text)
+        text_new = add_apostrophes (text_new.strftime ("%Y-%m-%d"))
+        return text_new
+    except:
+        raise ValueError ('Wrong date format.')
+
+def generate_report_query(date_min: str|None = None, date_max: str|None = None, category: str|None = None)->str:
+    query = Q_SELECT_ALL
+    counter = 0
+    if date_min != None or date_max != None or category != None:
+        query = query + ' WHERE '
+    if date_min != None:
+        date_min = convert_date_format (date_min)
+        counter +=1
+        query += Q_DATE_MIN + date_min
+    if date_max != None:
+        if counter > 0:
+            query += ' AND '
+        date_max = convert_date_format (date_max)
+        counter +=1
+        query += Q_DATE_MAX + date_max
+    if category != None:
+        if counter > 0:
+            query += ' AND '
+        query += Q_SELECT_BY_CATEGORY + add_apostrophes(category)
+    query += ';'
+    return query
+
+
+def generate_report (conn: MySQL_Connector, date_min: str|None = None, date_max: str|None = None, category: str|None = None)->None:
+    query = generate_report_query (date_min, date_max, category)
+    report = conn.cursor_execute(query)
+    print ('-ID- --=NAME=-- -=AMOUNT=- -=CATEGORY=- -=DATE=-')
+    for element in report:
+            print (f'{element[0]:3} {element[1]:13} {element[2]:9^} {element[3]:15} {element[4]:12}')
+
+@click.group()
+def cli():
+    pass
+    
+@cli.command()
+def create_database ()->None:
+    if RDBM == 'mysql':
+        conn = MySQL_Connector(MYSQL_CONFIG)
+    else:
+        conn = init_connection(RDBM)
+    try:
+        init_database(conn)
+        close_connection(conn)
+    except ValueError as e:
+        print (e)
+    try: 
+        conn = init_connection(RDBM)
+        init_table (conn) 
+        close_connection (conn)
+    except ValueError as e:
+        print (e)
+    
+    
+
+@cli.command()
+@click.argument ('name')
+@click.argument ('amount')
+@click.argument ('category')
+@click.argument ('date')
+def add (name: str, amount: float, category: str, date: str)->None:
+    conn = init_connection(RDBM)
+    try:
+        expense = create_single_expense (name, amount, category, date)
+        insert_values (conn, expense)
+    except ValueError as e:
+        print (e)
+        sys.exit(1)
+    close_connection (conn)
+
+
+@cli.command()
+@click.argument ('date_min', required=False)
+@click.argument ('date_max', required=False)
+@click.argument ('category', required=False)
+def report(date_min:str, date_max:str, category:str)->None:
+    conn = init_connection(RDBM)
+    generate_report(conn, date_min, date_max, category)
+    close_connection (conn)
+       
+
+if __name__ == "__main__":
+    cli()
+
---
+++ expense_tracker/SQL_Connectors.py
@@ -0,0 +1,141 @@
+import mysql.connector
+# from mysql.connector import errorcode
+import psycopg2
+import sqlite3
+
+
+Q_CREATE_TABLE_COMMAND =  'CREATE TABLE '
+Q_CREATE_TABLE_EXIST = 'IF NOT EXIST '
+Q_CREATE_TABLE_DESC = '''expenses_list
+                    (
+                    exp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR (30) NOT NULL, 
+                    amount DECIMAL(10,2) NOT NULL, 
+                    category VARCHAR (15) NOT NULL,
+                    date VARCHAR (50) NOT NULL
+                    );'''
+Q_CREATE_TABLE = Q_CREATE_TABLE_COMMAND + Q_CREATE_TABLE_DESC
+Q_CREATE_TABLE_SQLITE = Q_CREATE_TABLE_COMMAND + Q_CREATE_TABLE_EXIST + Q_CREATE_TABLE_DESC
+
+
+class Connector:
+    """
+    Base class for SQL connectors.
+    """
+
+    def cursor_execute(self, query: str, values=None):
+        """
+        Executes the given SQL query with the given parameter values.
+        """
+        if values is None:
+            values = ()
+        self.sqlcursor.execute(query, values)
+        if values != ():
+            self.connection.commit()
+        return self.sqlcursor.fetchall()
+
+
+class MySQL_Connector (Connector):
+    def __init__ (self, config):
+        """
+        Creates a new connection to a MySQL database.
+        """
+        try:
+            self.connection = mysql.connector.connect(**config)
+            self.sqlcursor = self.connection.cursor()
+        except mysql.connector.Error as e:
+            raise ValueError(f"Failed to connect to database: {e}")
+    
+
+    @classmethod
+    def create_database(cls, config: dict [str, str], query: str)->None:
+        '''Creates a new database in a MySQL server.'''
+        connection = mysql.connector.connect(**config)
+        sqlcursor = connection.cursor()
+        try:
+            sqlcursor.execute (query)
+            print ('Database "expenses" created.')
+            connection.close()
+            sqlcursor.close()
+        except mysql.connector.Error as e:
+            raise ValueError(f"Failed creating database: {e}")
+    
+    
+    def create_table(self)->None:
+        '''Creates a table in a MySQL server.'''
+        try:
+            self.cursor_execute(Q_CREATE_TABLE)
+            print ('Table "expenses_list" created.')
+        except mysql.connector.Error as e:
+            raise ValueError(f"Failed creating table: {e}")
+             
+
+class PostgreSQL_Connector (Connector):
+    def __init__(self, host: str, db: str, user: str, password: str)->None:
+        """
+        Creates a new connection to a PostgreSQL database.
+        """
+        try:
+            self.connection = psycopg2.connect(
+                host=host,
+                database=db,
+                user=user,
+                password=password
+            )
+            self.sqlcursor = self.connection.cursor()
+        except psycopg2.Error as e:
+            raise ValueError(f"Failed to connect to database: {e}")
+
+
+    @classmethod
+    def create_database(cls, config: dict [str, str], query: str)->None:
+        """
+        psycopg2 does not support 'CREATE DATABASE' query. 
+        You need to have the database already created in the PostgreSQL server 
+        before connecting to it using psycopg2.
+        """
+        raise ValueError ('Database with this name does not exist. Please log in to PostgreSQL client and create a database.')
+    
+    
+    def create_table(self)->None:
+        '''Creates a table in a MySQL server.'''
+        try:
+            self.cursor_execute(Q_CREATE_TABLE)
+            print ('Table "expenses_list" created.')
+        except mysql.connector.Error as e:
+            raise ValueError(f"Failed creating table: {e}")
+
+    
+class SQLite_Connector (Connector):
+    def __init__(self, db):
+        """
+        Creates a new connection to a SQLite database.
+        """
+        self.connection = sqlite3.connect(db)
+        self.sqlcursor = self.connection.cursor()
+
+
+    def cursor_execute(self, query: str, values: tuple = ()):
+        query_new = query.replace(r'%s', '?')
+        return super().cursor_execute(query_new, values)
+
+
+    @classmethod
+    def create_database(cls, config: dict [str, str], query: str)->None:
+        """
+        Does nothing because creating a database is not necessary in SQLite.
+        """
+        pass
+
+    
+    def create_table(self)->None:
+        '''Creates a table in a MySQL server.'''
+        try:
+            self.cursor_execute(Q_CREATE_TABLE_SQLITE)
+        except mysql.connector.Error as e:
+            raise ValueError(f"Failed creating table: {e}")
+
+
+
+
+
